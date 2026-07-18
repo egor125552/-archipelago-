8f0e7ea93017dd49173923b3f9314a85e4b5f589
egor125552 <sitkosergej55@gmail.com>
2026-07-18 16:22:13 +0300

    Reuse operation audio events in free roam

---
+++ public/src/free-roam-audio-v2.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import {FreeRoamAudio as BaseFreeRoamAudio} from "./free-roam-audio.js";
+
+export class FreeRoamAudio extends BaseFreeRoamAudio {
+  handleFreeEvent(event, playerIndex) {
+    if (event?.operationEvent && event?.targets?.includes(playerIndex)) {
+      this.handle([event]);
+      return;
+    }
+    super.handleFreeEvent(event, playerIndex);
+  }
+}
