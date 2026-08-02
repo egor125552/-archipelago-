a6a277771eaa8bf3000cb938758c5eab357c1aca
egor125552 <sitkosergej55@gmail.com>
2026-08-03 01:16:56 +0300

    Подключён единый координатный звук мегабомбы

--- public/src/free-roam-mega-bomb-client.js
+++ public/src/free-roam-mega-bomb-client.js
@@ -1,2 +1,2 @@
-export * from "./free-roam-mega-bomb-client-v23.js?v=2";
-import "./free-roam-mega-bomb-client-v23.js?v=2";
+export * from "./free-roam-mega-bomb-client-v24.js?v=1";
+import "./free-roam-mega-bomb-client-v24.js?v=1";
