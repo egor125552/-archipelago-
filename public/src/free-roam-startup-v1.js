e544ae24c3c6c094776a256604b208b558f60bc9
egor125552 <sitkosergej55@gmail.com>
2026-08-03 01:19:05 +0300

    Обновлена загрузка координатного клиента мегабомбы

--- public/src/free-roam-startup-v1.js
+++ public/src/free-roam-startup-v1.js
@@ -204,6 +204,6 @@ try {
   };
 })();
 
-import("./free-roam-mega-bomb-client.js?v=12").catch(error => {
+import("./free-roam-mega-bomb-client.js?v=13").catch(error => {
   console.error("Не удалось загрузить актуальный клиент мегабомбы", error);
 });
