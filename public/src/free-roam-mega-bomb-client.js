fa96fd7b983e2bb8b5781212628b819450a71244
egor125552 <sitkosergej55@gmail.com>
2026-08-03 11:21:41 +0300

    Подключён общий аудиодвижок мегабомбы

--- public/src/free-roam-mega-bomb-client.js
+++ public/src/free-roam-mega-bomb-client.js
@@ -1,2 +1,2 @@
-export * from "./free-roam-mega-bomb-client-v24.js?v=1";
-import "./free-roam-mega-bomb-client-v24.js?v=1";
+export * from "./free-roam-mega-bomb-client-v25.js?v=1";
+import "./free-roam-mega-bomb-client-v25.js?v=1";
