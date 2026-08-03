e8b25c41dbfaad5aab5b74830d514d901ada9743
egor125552 <sitkosergej55@gmail.com>
2026-08-03 21:12:13 +0300

    Подключена тактика подавления V170

--- public/src/free-roam-combat-ai-hotfix-v163.js
+++ public/src/free-roam-combat-ai-hotfix-v163.js
@@ -1,7 +1,7 @@
 "use strict";
 
 import {applyCombatAiHotfixV162} from "./free-roam-combat-ai-hotfix-v162.js?v=1";
-import {applyCombatAiModelV169} from "./free-roam-combat-ai-model-v169.js?v=1";
+import {applyCombatAiModelV170} from "./free-roam-combat-ai-model-v170.js?v=1";
 
 function ensureState(world) {
   world.freeCombatAiHotfixV163 ||= {encounterId: null, fixedOpeningActorIds: []};
@@ -54,5 +54,5 @@ function preserveOpeningActors(world) {
 export function applyCombatAiHotfixV163(world, dt, helpers = {}) {
   applyCombatAiHotfixV162(world, dt, helpers);
   preserveOpeningActors(world);
-  return applyCombatAiModelV169(world, dt, helpers);
+  return applyCombatAiModelV170(world, dt, helpers);
 }
