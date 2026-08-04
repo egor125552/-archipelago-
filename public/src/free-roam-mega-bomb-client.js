28d94bac38b8df9c769165ccac00012969ac56d6
egor125552 <sitkosergej55@gmail.com>
2026-08-04 15:54:09 +0300

    Сохранить движение хвоста мегабомбы без сетевого потока
    Обновлять панораму, расстояние и громкость длинного хвоста взрыва локально по текущему положению слушателя. Не возвращать серверный акустический поток и не отправлять WebSocket-сообщения. Подключить Safari cache-buster и регрессии локального сопровождения.

--- public/src/free-roam-mega-bomb-client.js
+++ public/src/free-roam-mega-bomb-client.js
@@ -1,2 +1,4 @@
-export * from "./free-roam-mega-bomb-client-v25.js?v=1";
-import "./free-roam-mega-bomb-client-v25.js?v=1";
+export * from "./free-roam-mega-bomb-client-v25.js?v=2";
+import "./free-roam-mega-bomb-client-v25.js?v=2";
+export * from "./free-roam-mega-bomb-impact-follow-v1.js?v=1";
+import "./free-roam-mega-bomb-impact-follow-v1.js?v=1";
