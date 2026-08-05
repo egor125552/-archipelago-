039251653a097be4b2730841e7989df38d54fb30
egor125552 <sitkosergej55@gmail.com>
2026-08-05 09:53:41 +0300

    Зафиксировать версию исправлений элитного катера

---
+++ public/src/free-roam-elite-boat-version-v1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+export const ELITE_BOAT_RELEASE = Object.freeze({
+  subsystem: "1.1.0",
+  boatAudioClass: 44,
+  bombReloadSeconds: 5,
+  physicalTurrets: true,
+  bombBayLifecycle: true,
+  developerLogEliteSnapshot: true,
+});
