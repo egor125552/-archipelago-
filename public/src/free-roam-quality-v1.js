8d0b35908ba1b8019bb6f1be08637f570a11000b
egor125552 <sitkosergej55@gmail.com>
2026-08-05 09:42:56 +0300

    Обновить кэш звука оставленной лодки

--- public/src/free-roam-quality-v1.js
+++ public/src/free-roam-quality-v1.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import "./free-roam-player-boat-audio-v1.js?v=1";
+import "./free-roam-player-boat-audio-v1.js?v=2";
 import {FreeRoamAudio} from "./free-roam-audio-v5.js?v=44";
 import {predictLocalWorld} from "./free-roam-client-prediction.js?v=41";
 import {
