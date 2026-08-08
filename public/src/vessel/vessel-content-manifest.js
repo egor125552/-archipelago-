d7b24435aa6fe9221adc93a88187eea1e4a09a36
egor125552 <sitkosergej55@gmail.com>
2026-08-08 21:08:43 +0300

    Bust vessel definition cache for walkable decks

--- public/src/vessel/vessel-content-manifest.js
+++ public/src/vessel/vessel-content-manifest.js
@@ -2,7 +2,7 @@
 
 import {installCoreVesselModuleTypes} from "./modules/core-module-types.js";
 import {installStressTestPhysicsModules} from "./physics/stress-test-physics.js?v=1";
-import {installCurrentVesselTypes} from "./definitions/current-vessels.js";
+import {installCurrentVesselTypes} from "./definitions/current-vessels.js?v=2";
 
 // Single explicit content extension point. Concrete module types, physics
 // modules and vessel definitions are registered here; generic free-roam code
