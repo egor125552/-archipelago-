a3899e128bb53922cb21ebbc815eddadbd385a00
egor125552 <sitkosergej55@gmail.com>
2026-08-08 23:01:50 +0300

    Register shared station input handling

--- public/src/vessel/vessel-plugin-manifest.js
+++ public/src/vessel/vessel-plugin-manifest.js
@@ -1,10 +1,11 @@
 "use strict";
 
-import {STRESS_TEST_VESSEL_SYSTEMS} from "./systems/stress-test-vessel-system.js?v=1";
+import {STRESS_TEST_VESSEL_SYSTEMS} from "./systems/stress-test-vessel-system.js?v=2";
 import {VESSEL_OWNERSHIP_SYSTEMS} from "./systems/vessel-ownership-system.js?v=1";
 import {VESSEL_DECK_BOARDING_SYSTEMS} from "./systems/vessel-deck-boarding-system.js?v=1";
 import {VESSEL_DECK_INPUT_BRIDGE_SYSTEMS} from "./systems/vessel-deck-input-bridge-system.js?v=1";
 import {VESSEL_DECK_LIFECYCLE_SYSTEMS} from "./systems/vessel-deck-lifecycle-system.js?v=1";
+import {VESSEL_STATION_INPUT_SYSTEMS} from "./systems/vessel-station-input-system.js?v=1";
 import {WALKABLE_VESSEL_SYSTEMS} from "./systems/walkable-vessel-system.js?v=1";
 import {VESSEL_DECK_ACCESSIBILITY_SYSTEMS} from "./systems/vessel-deck-accessibility-system.js?v=1";
 import {VESSEL_RESPAWN_SYSTEMS} from "./systems/vessel-respawn-system.js?v=1";
@@ -14,6 +15,7 @@ import {VESSEL_RESPAWN_SYSTEMS} from "./systems/vessel-respawn-system.js?v=1";
 export function installVesselPlugins(registry) {
   for (const system of VESSEL_DECK_BOARDING_SYSTEMS) registry.registerSystem(system);
   for (const system of VESSEL_DECK_INPUT_BRIDGE_SYSTEMS) registry.registerSystem(system);
+  for (const system of VESSEL_STATION_INPUT_SYSTEMS) registry.registerSystem(system);
   for (const system of WALKABLE_VESSEL_SYSTEMS) registry.registerSystem(system);
   for (const system of VESSEL_DECK_ACCESSIBILITY_SYSTEMS) registry.registerSystem(system);
   for (const system of VESSEL_DECK_LIFECYCLE_SYSTEMS) registry.registerSystem(system);
