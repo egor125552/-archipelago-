104a3e282c5147d61011ae3353208c01b205905c
egor125552 <sitkosergej55@gmail.com>
2026-08-10 12:32:32 +0300

    Load screen reader speech output support

--- public/src/free-roam-accessibility.js
+++ public/src/free-roam-accessibility.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import "./free-roam-pump-command-policy-v1.js?v=1";
+import "./free-roam-speech-output-mode.js?v=1";
 
 const shortcuts = Object.freeze({
   leftButton: {key: "ArrowLeft", code: "ArrowLeft", duration: 480},
